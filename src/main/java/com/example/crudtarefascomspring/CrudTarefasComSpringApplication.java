f03f439997a57934bc096575a27f252f56b27a7c
brunohmdev <brunohm.dev@gmail.com>
2026-03-03 23:45:35 -0300

    first commit

---
+++ src/main/java/com/example/crudtarefascomspring/CrudTarefasComSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudtarefascomspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudTarefasComSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudTarefasComSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/crudtarefascomspring/CrudTarefasComSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudtarefascomspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudTarefasComSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
